9ffcab2ef6252ae678e99e3221ba897b6ad300a1
Michael Waite <hotmailme1110@gmail.com>
2025-11-22 14:02:03 -0600

    Initialize package.json for backend project

---
+++ backend/package.js
@@ -0,0 +1,17 @@
+{
+  "name": "simple-rick-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "openai": "^4.10.0",
+    "dotenv": "^16.3.1",
+    "path": "^0.12.7",
+    "fs-extra": "^11.1.1"
+  }
+}
