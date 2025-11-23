ea20b128dfc4f912c2438df29f5954800e844fe1
Michael Waite <245466799+hotmailme1110-ai@users.noreply.github.com>
2025-11-22 20:21:20 -0600

    Update and rename simplerickAI/app/src/main.kt to app/src/main/java/com/yourname/simplerick/MainActivity.kt

--- simplerickAI/app/src/main.kt
+++
@@ -1,7 +0,0 @@
-SimpleRickAI/
- └─ app/
-     └─ src/
-         └─ main/
-             ├─ java/com/yourname/simplerick/   # for MainActivity.kt and FloatingService.kt
-             ├─ res/layout/                     # for XML layouts
-             └─ AndroidManifest.xml
